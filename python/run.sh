858345f13720603a9bca4e3e2ecfd20264981b93
Fil <fil@rezo.net>
2014-10-21 23:06:54 +0200

    python rather than ipython

--- python/run.sh
+++ python/run.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-for i in ../data/Database*.xlsx; do  yes ""|ipython main.py "$i"; done
+for i in ../data/Database*.xlsx; do  yes ""|python main.py "$i"; done
 cat ../data/*json | perl -pi -e 's/[]][[]/,/;' > ../../sample-maps/data/IRD.json 
       
